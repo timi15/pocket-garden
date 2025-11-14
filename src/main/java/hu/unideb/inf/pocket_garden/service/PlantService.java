3db261f33a1b50acbfd11d9784aed743a4f84431
timi15 <vargatimi505@gmail.com>
2025-11-14 19:01:38 +0100

    feat(plant): add POST & GET & DELETE mapping

---
+++ src/main/java/hu/unideb/inf/pocket_garden/service/PlantService.java
@@ -0,0 +1,21 @@
+package hu.unideb.inf.pocket_garden.service;
+
+import hu.unideb.inf.pocket_garden.service.dto.request.PlantReqDTO;
+import hu.unideb.inf.pocket_garden.service.dto.response.PlantResDTO;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PlantService {
+
+    PlantResDTO save(PlantReqDTO plantReqDTO);
+
+    List<PlantResDTO> findAll();
+
+    PlantResDTO findById(UUID id);
+
+    List<PlantResDTO> findByOwnerId(UUID ownerId);
+
+    PlantResDTO deleteById(UUID id);
+
+}
---
+++ src/main/java/hu/unideb/inf/pocket_garden/service/impl/PlantServiceImpl.java
@@ -0,0 +1,98 @@
+package hu.unideb.inf.pocket_garden.service.impl;
+
+import hu.unideb.inf.pocket_garden.data.entity.PlantEntity;
+import hu.unideb.inf.pocket_garden.data.repository.OwnerRepository;
+import hu.unideb.inf.pocket_garden.data.repository.PlantRepository;
+import hu.unideb.inf.pocket_garden.exception.AlreadyExistsException;
+import hu.unideb.inf.pocket_garden.exception.MissingOwnerException;
+import hu.unideb.inf.pocket_garden.exception.NotFoundException;
+import hu.unideb.inf.pocket_garden.mapper.PlantMapper;
+import hu.unideb.inf.pocket_garden.service.PlantService;
+import hu.unideb.inf.pocket_garden.service.dto.request.PlantReqDTO;
+import hu.unideb.inf.pocket_garden.service.dto.response.PlantResDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PlantServiceImpl implements PlantService {
+
+    private final PlantRepository plantRepository;
+    private final OwnerRepository ownerRepository;
+    private final PlantMapper plantMapper;
+
+    @Override
+    public PlantResDTO save(PlantReqDTO plantReqDTO) {
+
+        if (plantRepository.existsByNickname(plantReqDTO.getNickname())) {
+            throw new AlreadyExistsException(
+                    "Plant already exists with this nickname: "
+                            + plantReqDTO.getNickname()
+            );
+        }
+
+        if (null == plantReqDTO.getOwnerId()) {
+            throw new MissingOwnerException(
+                    "Owner ID is required to save a plant."
+            );
+        }
+
+        if (!ownerRepository.existsById(plantReqDTO.getOwnerId())) {
+            throw new NotFoundException(
+                    "Owner not found with id: "
+                            + plantReqDTO.getOwnerId()
+            );
+        }
+
+        if (null == plantReqDTO.getLastWateredAt()) {
+            plantReqDTO.setLastWateredAt(LocalDate.now());
+        }
+
+        PlantEntity plantEntity = plantMapper.toEntity(plantReqDTO);
+        plantEntity.setOwner(
+                ownerRepository
+                        .findById(plantReqDTO.getOwnerId()).get()
+        );
+
+        return plantMapper.toResponseDTO(plantRepository.save(plantEntity));
+    }
+
+    @Override
+    public List<PlantResDTO> findAll() {
+        List<PlantEntity> plantEntities = plantRepository.findAll();
+        return plantMapper.map(plantEntities);
+    }
+
+    @Override
+    public PlantResDTO findById(UUID id) {
+        if (!plantRepository.existsById(id)) {
+            throw new NotFoundException("Plant not found with id: " + id);
+        }
+        return plantMapper.toResponseDTO(plantRepository.findById(id).get());
+    }
+
+    @Override
+    public List<PlantResDTO> findByOwnerId(UUID ownerId) {
+        if (!ownerRepository.existsById(ownerId)) {
+            throw new NotFoundException("Owner not found with id: " + ownerId);
+        }
+        return plantMapper.map(plantRepository.findByOwnerId(ownerId));
+    }
+
+    @Override
+    public PlantResDTO deleteById(UUID id) {
+        if (!plantRepository.existsById(id)) {
+            throw new NotFoundException("Plant not found with id: " + id);
+        }
+
+        PlantEntity plantEntity = plantRepository.findById(id).get();
+        plantRepository.deleteById(id);
+
+        return plantMapper.toResponseDTO(plantEntity);
+    }
+
+}

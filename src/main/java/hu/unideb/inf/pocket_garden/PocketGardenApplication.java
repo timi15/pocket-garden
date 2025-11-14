03cb881e0b77dcd0d38b24d92c44b3f966e419b7
timi15 <vargatimi505@gmail.com>
2025-11-14 18:17:15 +0100

    Initial commit

---
+++ src/main/java/hu/unideb/inf/pocket_garden/PocketGardenApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.pocket_garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketGardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/unideb/inf/pocket_garden/PocketGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.pocket_garden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
